d1aeeb1831bf53e7c8b89d1c4c6a41d215477edc
glitterlungs <alijagruca6@gmail.com>
2022-04-19 01:55:02 +0200

    Add MahApp, functionality of modify and delete buttons

--- QuizGenerator/SubWindow.xaml.cs
+++ QuizGenerator/SubWindow.xaml.cs
@@ -25,9 +25,189 @@ namespace QuizGenerator
             InitializeComponent();
         }
 
+        // konstruktor by przekazac nazwe quizu z MainWindow
         public SubWindow(string quizNameText) : this()
         {
             quizName.Text = quizNameText;
+        }
+
+        // obsluga przycisku Add
+        private void addButtonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // sprawdzenie czy wszystkie textBoxy zostały wypełnione
+                if (isNoEmpty(questionTextBox) & isNoEmpty(answerOne) & isNoEmpty(answerTwo) & isNoEmpty(answerThree) & isNoEmpty(answerFour))
+                {
+                    // stworzenie listy odpowiedzi poprawnych
+                    List<string> correct = new List<string>();
+
+                    if (answerOneCorrect.IsChecked == true)
+                    {
+                        correct.Add(answerOne.Text);
+                    }
+                    if (answerTwoCorrect.IsChecked == true)
+                    {
+                        correct.Add(answerTwo.Text);
+                    }
+                    if (answerThreeCorrect.IsChecked == true)
+                    {
+                        correct.Add(answerThree.Text);
+                    }
+                    if (answerFourCorrect.IsChecked == true)
+                    {
+                        correct.Add(answerFour.Text);
+                    }
+
+                    // sprawdzenie czy zaznaczono jakas prawidlowa odpowiedz
+                    if (!correct.Any())
+                    {
+                        MessageBox.Show("Select the correct answer/answers using checkboxes.");
+                    }
+                    else
+                    {
+                        Question question = new Question(questionTextBox.Text, answerOne.Text, answerTwo.Text, answerThree.Text, answerFour.Text, correct);
+
+                        listBox.Items.Add(question);
+                        listBox.SelectedItem = null;
+
+                        clearTextBoxes();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            
+        }
+
+        // obsługa przycisku Modify
+        private void modifyButtonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Question selectedQuestion = listBox.SelectedItem as Question;
+                if(selectedQuestion != null)
+                {
+                    if (isNoEmpty(questionTextBox) & isNoEmpty(answerOne) & isNoEmpty(answerTwo) & isNoEmpty(answerThree) & isNoEmpty(answerFour))
+                    {
+                        // stworzenie listy odpowiedzi poprawnych
+                        List<string> correct = new List<string>();
+
+                        if (answerOneCorrect.IsChecked == true)
+                        {
+                            correct.Add(answerOne.Text);
+                        }
+                        if (answerTwoCorrect.IsChecked == true)
+                        {
+                            correct.Add(answerTwo.Text);
+                        }
+                        if (answerThreeCorrect.IsChecked == true)
+                        {
+                            correct.Add(answerThree.Text);
+                        }
+                        if (answerFourCorrect.IsChecked == true)
+                        {
+                            correct.Add(answerFour.Text);
+                        }
+
+                        // sprawdzenie czy zaznaczono jakas prawidlowa odpowiedz
+                        if (!correct.Any())
+                        {
+                            MessageBox.Show("Select the correct answer/answers using checkboxes.");
+                        }
+                        else
+                        {
+                            selectedQuestion.Name = questionTextBox.Text;
+                            selectedQuestion.answerOne = answerOne.Text;
+                            selectedQuestion.answerTwo = answerTwo.Text;
+                            selectedQuestion.answerThree = answerThree.Text;
+                            selectedQuestion.answerFour = answerFour.Text;
+                            selectedQuestion.correctAnswers = correct;
+
+                            listBox.Items.Refresh();
+                            listBox.SelectedItem = null;
+
+                            clearTextBoxes();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // obsługa przycisku Delete
+        private void deleteButtonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                listBox.Items.Remove(listBox.SelectedItem);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        // po wybraniu danego pola w listBox zostaje ono wpisane w formularz
+        private void listBoxSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                Question selectedQuestion = listBox.SelectedItem as Question; 
+                if (selectedQuestion != null)
+                {
+                    questionTextBox.Text = selectedQuestion.Name;
+                    answerOne.Text = selectedQuestion.answerOne;
+                    answerTwo.Text = selectedQuestion.answerTwo;
+                    answerThree.Text = selectedQuestion.answerThree;
+                    answerFour.Text = selectedQuestion.answerFour;
+
+                    if (selectedQuestion.correctAnswers.Contains(answerOne.Text))
+                    {
+                        answerOneCorrect.IsChecked = true;
+                    }
+                    if (selectedQuestion.correctAnswers.Contains(answerTwo.Text))
+                    {
+                        answerTwoCorrect.IsChecked = true;
+                    }
+                    if (selectedQuestion.correctAnswers.Contains(answerThree.Text))
+                    {
+                        answerThreeCorrect.IsChecked = true;
+                    }
+                    if (selectedQuestion.correctAnswers.Contains(answerFour.Text))
+                    {
+                        answerFourCorrect.IsChecked = true;
+                    }
+                }
+                else
+                {
+                    clearTextBoxes();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void clearTextBoxes()
+        {
+            questionTextBox.Text = "";
+            answerOne.Text = "";
+            answerTwo.Text = "";
+            answerThree.Text = "";
+            answerFour.Text = "";
+            answerOneCorrect.IsChecked = false;
+            answerThreeCorrect.IsChecked = false;
+            answerThreeCorrect.IsChecked = false;
+            answerFourCorrect.IsChecked = false;
         }
 
         private bool isNoEmpty(TextBoxWithErrorProvider tb)
@@ -39,46 +219,6 @@ namespace QuizGenerator
             }
             tb.SetError("");
             return true;
-        }
-
-        private void addButtonClick(object sender, RoutedEventArgs e)
-        {
-            if(isNoEmpty(questionTextBox) & isNoEmpty(answerOne) & isNoEmpty(answerTwo) & isNoEmpty(answerThree) & isNoEmpty(answerFour))
-            {
-                List<string> correct = new List<string>();
-
-                if (answerOneCorrect.IsChecked == true)
-                {
-                    correct.Add(answerOne.Text);
-                }
-                if (answerTwoCorrect.IsChecked == true)
-                {
-                    correct.Add(answerTwo.Text);
-                }
-                if (answerThreeCorrect.IsChecked == true)
-                {
-                    correct.Add(answerThree.Text);
-                }
-                if (answerFourCorrect.IsChecked == true)
-                {
-                    correct.Add(answerFour.Text);
-                }
-
-                Question question = new Question(questionTextBox.Text, answerOne.Text, answerTwo.Text, answerThree.Text, answerFour.Text, correct);
-
-                listBox.Items.Add(question);
-                listBox.SelectedItem = null;
-
-                questionTextBox.Text = "";
-                answerOne.Text = "";
-                answerTwo.Text = "";
-                answerThree.Text = "";
-                answerFour.Text = "";
-                answerOneCorrect.IsChecked = false;
-                answerThreeCorrect.IsChecked = false;
-                answerThreeCorrect.IsChecked = false;
-                answerFourCorrect.IsChecked = false;
-            }
         }
     }
 }
